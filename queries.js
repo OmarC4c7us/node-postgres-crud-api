58e37d9002d3ea80fed06d1d6c2cee30d3ceb579
Omar <126634376+OmarC4c7us@users.noreply.github.com>
2023-03-01 03:03:25 +0000

    change pg vars to envs

--- queries.js
+++ queries.js
@@ -1,11 +1,20 @@
 const Pool = require('pg').Pool;
+const db_user = process.env.PG_USER || 'me';
+const db_host = process.env.PG_HOST || 'localhost';
+const db_name = process.env.DB_NAME || 'api2';
+const db_password = process.env.PASSWORD || '12345';
+const db_port = process.env.PG_USER || 5432;
+
+console.log(db_user);
+
 const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'api2',
-  password: '12345',
-  port: 5432,
+  user: db_user,
+  host: db_host,
+  database: db_name,
+  password: db_password,
+  port: db_port
 });
+
 const getUsers = (request, response) => {
   pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
     if (error) {
